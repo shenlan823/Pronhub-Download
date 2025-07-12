505a928394f4d8c23c65af3f45c9372d20228585
shenlan823 <xixixi951@gmail.com>
2025-07-12 17:16:54 +0800

    main

---
+++ Pornhub-Video-Downloader-Plugin-V3.0/README.md
@@ -0,0 +1,11 @@
+# Pornhub 视频下载插件
+
+由于 Chrome 更新 `Manifest V3` 后，部分函数无法使用，所以将部分语法更新了一下。
+
+xvideos 的下载部分不受影响，但是 pronhub 部分使用了 `eval(js)` 所以我重新写了一下。
+
+但是我发现有一个问题，就是 pronhub 有的视频无法下载，有的视频又能正常下载，而且我用之前的 **v2.1** 版本也无法下载，不知道是我写的有问题还是啥问题。**有人知道能解答一下吗？**
+
+原作者的 github 账号好像没了，[zgao https://zgao.top/](https://zgao.top/)这是博客。
+
+有需要可以联系原作者[https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=zgao264@gmail.com](https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=zgao264@gmail.com)
---
+++ Pornhub-Video-Downloader-Plugin-V3.0/popup.js
@@ -0,0 +1,57 @@
+﻿function sendMessageToContentScript(message, callback) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, message, function (response) {
+      if (callback) callback(response);
+    });
+  });
+}
+
+sendMessageToContentScript(
+  { cmd: "test", value: "test" },
+  function (videoType) {
+    if (!videoType) return;
+
+    const boxEl = document.getElementsByTagName("ul")[0];
+    let videoStr = "";
+
+    videoType.forEach((item) => {
+      videoStr += `<li>
+        <label>清晰度：<span>${item.key}</span></label>
+        <button class="button down">下载</button>
+        <button class="button copy">复制</button>
+      </li>`;
+    });
+
+    boxEl.innerHTML = videoStr;
+
+    const dialog = document.getElementById("showDialog");
+    const dialog1 = document.getElementById("showDialog1");
+
+    document.querySelectorAll(".down").forEach((item, index) => {
+      item.onclick = () => {
+        const reg = /[\~\.\:\/\*\?\"\|\\\<\>]/g;
+        chrome.downloads.download({
+          url: videoType[index].val,
+          filename: videoType[index].video_title.replace(reg, "") + ".mp4",
+        });
+
+        dialog1.showModal();
+        setTimeout(() => dialog1.close(), 2000);
+      };
+    });
+
+    document.querySelectorAll(".copy").forEach((item, index) => {
+      item.onclick = () => {
+        const url = videoType[index].val;
+        const oInput = document.createElement("input");
+        oInput.value = url;
+        document.body.appendChild(oInput);
+        oInput.select();
+        document.execCommand("Copy");
+        oInput.remove();
+        dialog.showModal();
+        setTimeout(() => dialog.close(), 1500);
+      };
+    });
+  }
+);
